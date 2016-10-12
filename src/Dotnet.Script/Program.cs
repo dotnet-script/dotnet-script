d33079cfcb268cc363d151527bce1a819241276e
Atif Aziz <code@raboof.com>
2016-10-12 11:22:35 +0200

    Don't crash on errors

--- src/Dotnet.Script/Program.cs
+++ src/Dotnet.Script/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -33,7 +33,28 @@ namespace Dotnet.Script
             "System.Collections.Generic"
         };
 
-        public static void Main(string[] args)
+        const string DebugFlagShort = "-d";
+        const string DebugFlagLong = "--debug";
+
+        public static int Main(string[] args)
+        {
+            try
+            {
+                return Wain(args);
+            }
+            catch (Exception e)
+            {
+                // Be verbose (stack trace) in debug mode otherwise brief
+                var error = args.Any(arg => arg == DebugFlagShort
+                                         || arg == DebugFlagLong)
+                          ? e.ToString()
+                          : e.GetBaseException().Message;
+                Console.Error.WriteLine(error);
+                return 0xbad;
+            }
+        }
+
+        private static int Wain(string[] args)
         {
             var commandLineApplication = new CommandLineApplication(throwOnUnexpectedArg: false);
 
@@ -41,7 +62,7 @@ namespace Dotnet.Script
 
             var scriptArgs = commandLineApplication.Option("-a |--arg <args>", "Arguments to pass to a script. Multiple values supported", CommandOptionType.MultipleValue);
             var config = commandLineApplication.Option("-c |--configuration <configuration>", "Configuration to use. Defaults to 'Release'", CommandOptionType.SingleValue);
-            var debugMode = commandLineApplication.Option("-d |--debug", "Enables debug output.", CommandOptionType.NoValue);
+            var debugMode = commandLineApplication.Option(DebugFlagShort + " | " + DebugFlagLong, "Enables debug output.", CommandOptionType.NoValue);
 
             commandLineApplication.HelpOption("-? | -h | --help");
             commandLineApplication.OnExecute(() =>
@@ -53,7 +74,7 @@ namespace Dotnet.Script
                 return 0;
             });
 
-            commandLineApplication.Execute(args);
+            return commandLineApplication.Execute(args);
         }
 
         private static void RunScript(string file, string config, bool debugMode, List<string> scriptArgs)
