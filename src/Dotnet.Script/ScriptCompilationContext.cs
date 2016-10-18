6ea5914d60feb04eea634ac3ad24f13af94f22a3
Atif Aziz <code@raboof.com>
2016-10-18 18:57:59 +0200

    ScriptCompilationContext.Compilation is redundant
    The Compilation object is available via ScriptCompilationContext.Script.GetCompilation.

--- src/Dotnet.Script/ScriptCompilationContext.cs
+++ src/Dotnet.Script/ScriptCompilationContext.cs
@@ -1,4 +1,3 @@
-using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.Scripting;
 using Microsoft.CodeAnalysis.Scripting.Hosting;
 using Microsoft.CodeAnalysis.Text;
@@ -7,15 +6,13 @@ namespace Dotnet.Script
 {
     public class ScriptCompilationContext<TReturn>
     {
-        public Compilation Compilation { get; }
         public Script<TReturn> Script { get; }
         public InteractiveScriptGlobals Host { get; }
         public SourceText SourceText { get; }
         public InteractiveAssemblyLoader Loader { get; }
 
-        public ScriptCompilationContext(Compilation compilation, Script<TReturn> script, InteractiveScriptGlobals host, SourceText sourceText, InteractiveAssemblyLoader loader)
+        public ScriptCompilationContext(Script<TReturn> script, InteractiveScriptGlobals host, SourceText sourceText, InteractiveAssemblyLoader loader)
         {
-            Compilation = compilation;
             Script = script;
             Host = host;
             SourceText = sourceText;
