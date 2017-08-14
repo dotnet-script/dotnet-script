04f0065aaa9d621270964f525c6dead61c44d96c
filipw <wojcieszyn@gmail.com>
2017-08-14 10:06:21 +1000

    added missing files

---
+++ src/Dotnet.Script.Tests/ProcessHelper.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace Dotnet.Script.Tests
+{
+    public static class ProcessHelper
+    {
+        public static string RunAndCaptureOutput(string fileName, string[] arguments, string workingDirectory = null)
+        {
+            var startInfo = new ProcessStartInfo(fileName, string.Join(" ", arguments))
+            {
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+                CreateNoWindow = true,
+                //UseShellExecute = false,
+                WorkingDirectory = workingDirectory ?? Directory.GetCurrentDirectory()
+            };
+
+            var process = new Process
+            {
+                StartInfo = startInfo
+            };
+
+            try
+            {
+                process.Start();
+            }
+            catch
+            {
+                Console.WriteLine($"Failed to launch '{fileName}' with args, '{arguments}'");
+                return null;
+            }
+
+            var output = process.StandardOutput.ReadToEnd();
+            output += process.StandardError.ReadToEnd();
+
+            process.WaitForExit();
+
+            return output.Trim();
+        }
+    }
+}
