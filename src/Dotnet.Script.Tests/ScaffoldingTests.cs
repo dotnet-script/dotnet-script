f18865f704ec1c8217c0e8ef331a5744b433ffa0
Bernhard Richter <bernhard.richter@gmail.com>
2017-08-30 15:45:05 +0200

    Added support for simple scaffolding

---
+++ src/Dotnet.Script.Tests/ScaffoldingTests.cs
@@ -0,0 +1,67 @@
+﻿using System.IO;
+using Xunit;
+
+namespace Dotnet.Script.Tests
+{
+    public class ScaffoldingTests
+    {
+        [Fact]
+        public void ShouldInitializeScriptFolder()
+        {            
+            var tempFolder = CreateTempFolder();
+            var result = Execute("init", tempFolder);
+            Assert.Equal(0, result.exitCode);
+            Assert.Contains(Path.Combine(tempFolder, "helloworld.csx"), Directory.GetFiles(tempFolder));
+            Assert.Contains(Path.Combine(tempFolder, "omnisharp.json"), Directory.GetFiles(tempFolder));
+            Assert.Contains(Path.Combine(tempFolder, ".vscode/launch.json"), Directory.GetFiles(tempFolder));
+            Directory.Delete(tempFolder,true);
+        }
+
+        [Fact]
+        public void ShouldCreateNewScript()
+        {
+            //var result = ExecuteInProcess("new", "script.csx");
+            var tempFolder = CreateTempFolder();
+            var result = Execute("new script.csx", tempFolder);
+            Assert.Equal(0, result.exitCode);
+            Assert.Contains(Path.Combine(tempFolder, "script.csx"), Directory.GetFiles(tempFolder));            
+            Directory.Delete(tempFolder, true);
+        }
+
+
+        private static string CreateTempFolder()
+        {
+            var userTempFolder = Path.GetTempPath();
+            var tempFile = Path.GetFileNameWithoutExtension(Path.GetTempFileName());
+            var tempFolder = Path.Combine(userTempFolder, tempFile);
+            Directory.CreateDirectory(tempFolder);
+            return tempFolder;
+        }
+
+        /// <summary>
+        /// Use this if you need to debug.
+        /// </summary>        
+        private static int ExecuteInProcess(params string[] args)
+        {            
+            return Program.Main(args);
+        }
+
+        private static (string output, int exitCode) Execute(string args, string workingDirectory)
+        {
+            var result = ProcessHelper.RunAndCaptureOutput("dotnet", GetDotnetScriptArguments(args), workingDirectory);
+            return result;
+        }
+
+        private static string[] GetDotnetScriptArguments(string args)
+        {
+            string configuration;
+#if DEBUG
+            configuration = "Debug";
+#else
+            configuration = "Release";
+#endif
+            return new[] { "exec", Path.Combine(Directory.GetCurrentDirectory(), "..", "..", "..", "..", "Dotnet.Script", "bin", configuration, "netcoreapp1.1", "dotnet-script.dll"), args };
+        }
+    }
+
+}
