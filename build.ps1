e3a658b4ab84ff814566e6b26bdf814b41bf478e
Bernhard Richter <bernhard.richter@gmail.com>
2017-09-07 12:50:07 +0200

    Fixed some root paths

--- build.ps1
+++ build.ps1
@@ -2,7 +2,7 @@ $scriptRoot = Split-Path $MyInvocation.MyCommand.Path -Parent
 $dotnetScriptPath = "$scriptRoot\build\dotnet-script"
 if (!(Test-Path $dotnetScriptPath)) {
     Write-Host "Downloading dotnet-script..."
-    Invoke-Expression ".\install-dotnet-script.ps1"
+    Invoke-Expression "$scriptRoot\build\install-dotnet-script.ps1"
 }
 
 dotnet "$dotnetScriptPath\dotnet-script.dll" "$scriptRoot\build\build.csx" -- $scriptRoot
