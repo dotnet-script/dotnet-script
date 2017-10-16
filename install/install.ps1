c080e00b78622873eb0e5191597c6638c555f9d8
Bernhard Richter <bernhard.richter@gmail.com>
2017-10-16 21:55:07 +0200

    Added install script for Windows(PowerShell)

---
+++ install/install.ps1
@@ -0,0 +1,33 @@
+# Create a temporary folder to download to.
+$tempFolder = Join-Path $env:TEMP "dotnet-script"
+New-Item $tempFolder -ItemType Directory -Force
+
+# Download the zip
+$client = New-Object "System.Net.WebClient"
+$url = "https://github.com/filipw/dotnet-script/releases/download/0.14.0/dotnet-script.0.14.0.zip"
+$zipFile = Join-Path $tempFolder "dotnet-script.zip"
+$client.DownloadFile($url,$zipFile)
+
+$installationFolder = Join-Path $env:ProgramData "dotnet-script"
+Expand-Archive $zipFile -DestinationPath $installationFolder -Force
+
+$path = [System.Environment]::GetEnvironmentVariable("path", [System.EnvironmentVariableTarget]::User);
+# Get all paths except paths to old dotnet.script installations. 
+$paths = $path.Split(";") -inotlike "*dotnet.script*" 
+# Add the installation folder to the path
+$paths += Join-Path $installationFolder "dotnet-script"
+# Create the new path string
+$path = $paths -join ";"
+
+[System.Environment]::SetEnvironmentVariable("path", $path, [System.EnvironmentVariableTarget]::User)
+
+
+
+
+
+
+
+
+
+
+
