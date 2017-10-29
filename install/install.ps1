1efd3542cd23d8baa0db241e9c7485ecdf0afe23
Bernhard Richter <bernhard.richter@gmail.com>
2017-10-29 16:42:36 +0100

    Install latest version(install.ps1)

--- install/install.ps1
+++ install/install.ps1
@@ -2,9 +2,16 @@
 $tempFolder = Join-Path $env:TEMP "dotnet-script"
 New-Item $tempFolder -ItemType Directory -Force
 
+# Get the latest release
+$latestRelease = Invoke-WebRequest "https://api.github.com/repos/filipw/dotnet-script/releases/latest" |
+ConvertFrom-Json |
+Select-Object tag_name
+$tag_name =  $latestRelease.tag_name
+
 # Download the zip
+Write-Host "Downloading latest verson ($tag_name)"
 $client = New-Object "System.Net.WebClient"
-$url = "https://github.com/filipw/dotnet-script/releases/download/0.14.0/dotnet-script.0.14.0.zip"
+$url = "https://github.com/filipw/dotnet-script/releases/download/$tag_name/dotnet-script.$tag_name.zip"
 $zipFile = Join-Path $tempFolder "dotnet-script.zip"
 $client.DownloadFile($url,$zipFile)
 
