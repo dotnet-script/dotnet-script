c71322cc42eac21996bdb11639be67e22bc6b31d
Bernhard Richter <bernhard.richter@gmail.com>
2017-11-21 20:21:04 +0100

    fixes #167

--- src/Dotnet.Script.Tests/ScriptProjectProviderTests.cs
+++ src/Dotnet.Script.Tests/ScriptProjectProviderTests.cs
@@ -1,4 +1,5 @@
 ﻿using System.IO;
+using System.Text;
 using Dotnet.Script.DependencyModel.ProjectSystem;
 using Xunit;
 using Xunit.Abstractions;
@@ -21,6 +22,18 @@ namespace Dotnet.Script.Tests
             var pathToProjectFile = provider.CreateProject(TestPathUtils.GetFullPathToTestFixture("LocalNuGetConfig"), "netcoreapp2.0", true);
             var pathToProjectFileFolder = Path.GetDirectoryName(pathToProjectFile);
             Assert.True(File.Exists(Path.Combine(pathToProjectFileFolder,"NuGet.Config")));
+        }
+
+        [Fact]
+        public void ShouldLogProjectFileContent()
+        {
+            StringBuilder log = new StringBuilder();            
+            var provider = new ScriptProjectProvider(type => ((level, message) => log.AppendLine(message)));
+
+            provider.CreateProject(TestPathUtils.GetFullPathToTestFixture("Helloworld"), "netcoreapp2.0", true);
+            var output = log.ToString();
+
+            Assert.Contains("<Project Sdk=\"Microsoft.NET.Sdk\">",output);
         }
 
         private ScriptProjectProvider CreateProvider()
