a2a0c2162427c396863311f97e090721b2895846
Bernhard Richter <bernhard.richter@gmail.com>
2017-11-21 11:09:44 +0100

    Split RuntimeDependency and RuntimeAssembly into separate files

--- src/Dotnet.Script.DependencyModel/Runtime/RuntimeDependency.cs
+++ src/Dotnet.Script.DependencyModel/Runtime/RuntimeDependency.cs
@@ -1,36 +1,7 @@
 ﻿using System.Collections.Generic;
-using System.Reflection;
 
 namespace Dotnet.Script.DependencyModel.Runtime
 {
-    public class RuntimeAssembly
-    {
-        public AssemblyName Name { get; }
-        public string Path { get; }       
-
-        public RuntimeAssembly(AssemblyName name, string path)
-        {
-            Name = name;
-            Path = path;
-        }
-                
-        public override int GetHashCode()
-        {
-            return Name.GetHashCode() ^ Path.GetHashCode();
-        }
-
-        public override bool Equals(object obj)
-        {
-            var other = (RuntimeAssembly)obj;
-            return other.Name == Name && other.Path == Path;
-        }
-        
-        public override string ToString()
-        {
-            return $"{nameof(Name)}: {Name}, {nameof(Path)}: {Path}";
-        }
-    }
-
     public class RuntimeDependency
     {
         public RuntimeDependency(string name, string version, IReadOnlyList<RuntimeAssembly> assemblies, IReadOnlyList<string> nativeAssets, IReadOnlyList<string> scripts)
