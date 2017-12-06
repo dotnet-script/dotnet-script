9649df80fb758b160311b5599e6c3690ed3bad96
seesharper <bernhard.richter@gmail.com>
2017-12-06 13:57:59 +0100

    Added support specifying version when installing on *nix

--- install/install.sh
+++ install/install.sh
@@ -1,5 +1,19 @@
+#!/bin/bash
 mkdir /tmp/dotnet-script
-version=$(curl https://api.github.com/repos/filipw/dotnet-script/releases/latest | grep -Eo "\"tag_name\":\s*\"(.*)\"" | cut -d'"' -f4)
+if [[ -z $1 ]]; then
+    version=$(curl https://api.github.com/repos/filipw/dotnet-script/releases/latest | grep -Eo "\"tag_name\":\s*\"(.*)\"" | cut -d'"' -f4)
+else
+    version=$1
+fi    
+currentVersion=$(dotnet script -v | tr -d '\n') 
+
+if [[ $? -eq 0 ]]; then
+    if [ "$version" == "$currentVersion" ]; then
+        echo $version already installed
+        exit 0
+    fi   
+fi
+
 echo "Installing $version..."
 curl -L https://github.com/filipw/dotnet-script/releases/download/$version/dotnet-script.$version.zip > /tmp/dotnet-script/dotnet-script.zip
 unzip -o /tmp/dotnet-script/dotnet-script.zip -d /usr/local/lib
@@ -7,3 +21,4 @@ chmod +x /usr/local/lib/dotnet-script/dotnet-script.sh
 cd /usr/local/bin
 ln -sfn /usr/local/lib/dotnet-script/dotnet-script.sh dotnet-script
 rm -rf /tmp/dotnet-script
+echo Installation Finished
