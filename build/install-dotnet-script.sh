5077934976cc3e87f35770bef19b614a28dcac05
filipw <wojcieszyn@gmail.com>
2017-12-12 08:43:50 +0100

    use 0.17.0 for build

--- build/install-dotnet-script.sh
+++ build/install-dotnet-script.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-curl -L https://github.com/filipw/dotnet-script/releases/download/0.14.0/dotnet-script.0.14.0.zip > dotnet-script.zip
+curl -L https://github.com/filipw/dotnet-script/releases/download/0.17.0/dotnet-script.0.17.0.zip > dotnet-script.zip
 unzip -o dotnet-script.zip -d ./
