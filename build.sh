3b11580f72416753a65b7a645b6a65330cc11332
andmos <andreas.mosti@gmail.com>
2018-01-02 15:57:39 +0100

    picking latest version of dotnet-script when building. Fixing #213
    debugging travisOSX build
    
    undoing debug statement
    
    picking latest version of dotnet-script when building. Fixing #213
    
    debugging travisOSX build

--- build.sh
+++ build.sh
@@ -3,8 +3,13 @@
 SCRIPT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 DOTNET_SCRIPT="$SCRIPT_DIR/build/dotnet-script"
 if [ ! -d "$DOTNET_SCRIPT" ]; then
-    echo "Downloading dotnet-script..."
-    curl -L https://github.com/filipw/dotnet-script/releases/download/0.17.0/dotnet-script.0.17.0.zip > "$SCRIPT_DIR/build/dotnet-script.zip"
+    currentVersion=$(curl https://api.github.com/repos/filipw/dotnet-script/releases/latest | grep -Eo "\"tag_name\":\s*\"(.*)\"" | cut -d'"' -f4)
+
+    echo Debug.... 
+    curl --version 
+    curl https://api.github.com/repos/filipw/dotnet-script/releases/latest 
+    echo "Downloading dotnet-script version $currentVersion..."
+    curl -L https://github.com/filipw/dotnet-script/releases/download/$currentVersion/dotnet-script.$currentVersion.zip > "$SCRIPT_DIR/build/dotnet-script.zip"
     unzip -o "$SCRIPT_DIR/build/dotnet-script.zip" -d "$SCRIPT_DIR/build/"
     if [ $? -ne 0 ]; then
         echo "An error occured while downloading dotnet-script"
