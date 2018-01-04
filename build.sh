c52983d95be6079c4d30d7fa1ca33e95aeabf9ea
andmos <andreas.mosti@gmail.com>
2018-01-04 12:35:08 +0100

    added token for github api
    removed debug statement

--- build.sh
+++ build.sh
@@ -3,11 +3,7 @@
 SCRIPT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 DOTNET_SCRIPT="$SCRIPT_DIR/build/dotnet-script"
 if [ ! -d "$DOTNET_SCRIPT" ]; then
-    currentVersion=$(curl https://api.github.com/repos/filipw/dotnet-script/releases/latest | grep -Eo "\"tag_name\":\s*\"(.*)\"" | cut -d'"' -f4)
-
-    echo Debug.... 
-    curl --version 
-    curl https://api.github.com/repos/filipw/dotnet-script/releases/latest 
+    currentVersion=$(curl https://api.github.com/repos/filipw/dotnet-script/releases/latest?access_token=3a5de576bd32ddfccb52662d2d08d33a7edc318b | grep -Eo "\"tag_name\":\s*\"(.*)\"" | cut -d'"' -f4)
     echo "Downloading dotnet-script version $currentVersion..."
     curl -L https://github.com/filipw/dotnet-script/releases/download/$currentVersion/dotnet-script.$currentVersion.zip > "$SCRIPT_DIR/build/dotnet-script.zip"
     unzip -o "$SCRIPT_DIR/build/dotnet-script.zip" -d "$SCRIPT_DIR/build/"
