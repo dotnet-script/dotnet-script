5b7e9fc89d6a9fb2d5fb13e98207c159ae515202
Bernhard Richter <bernhard.richter@gmail.com>
2018-04-26 16:47:59 +0200

    Use lock free file reads

--- src/Dotnet.Script.DependencyModel/ProjectSystem/ScriptFilesResolver.cs
+++ src/Dotnet.Script.DependencyModel/ProjectSystem/ScriptFilesResolver.cs
@@ -39,7 +39,7 @@ namespace Dotnet.Script.DependencyModel.ProjectSystem
 
         private static string[] GetLoadDirectives(string csxFile)
         {
-            var content = ReadFile(csxFile);
+            var content = FileUtils.ReadFile(csxFile);
             var matches = Regex.Matches(content, @"^\s*#load\s*""\s*(.+)\s*""", RegexOptions.IgnoreCase | RegexOptions.Multiline);
             List<string> result = new List<string>();
             foreach (var match in matches.Cast<Match>())
@@ -53,18 +53,6 @@ namespace Dotnet.Script.DependencyModel.ProjectSystem
             }
 
             return result.ToArray();
-        }
-
-
-        private static string ReadFile(string pathToFile)
-        {
-            using (var fileStream = new FileStream(pathToFile, FileMode.Open))
-            {
-                using (var reader = new StreamReader(fileStream))
-                {
-                    return reader.ReadToEnd();
-                }
-            }
         }
     }
 }
