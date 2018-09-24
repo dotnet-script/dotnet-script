4de11793b1f5401203ef8bb3e077092e993236d9
Tom Laird-McConnell <tomlm@microsoft.com>
2018-09-24 12:16:08 -0700

    Speed up dotnet restore by * not executing _commandRunner to check for existence of dotnet (dotnet-script is already running as a dotnet tool) * computing a project hash so we only have to run dotnet restore once per project configuration.  If runtime or nuget package dependencies change, it will run dotnet restore, otherwise it will be skipped.

--- src/Dotnet.Script.DependencyModel/Context/DotnetRestorer.cs
+++ src/Dotnet.Script.DependencyModel/Context/DotnetRestorer.cs
@@ -1,8 +1,10 @@
-﻿using System;
-using System.Linq;
-using Dotnet.Script.DependencyModel.Environment;
+﻿using Dotnet.Script.DependencyModel.Environment;
 using Dotnet.Script.DependencyModel.Logging;
 using Dotnet.Script.DependencyModel.Process;
+using System;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
 
 namespace Dotnet.Script.DependencyModel.Context
 {
@@ -20,9 +22,25 @@ namespace Dotnet.Script.DependencyModel.Context
         }
 
         public void Restore(string pathToProjectFile, string[] packageSources)
-        {           
+        {
             var packageSourcesArgument = CreatePackageSourcesArguments();
             var runtimeIdentifier = _scriptEnvironment.RuntimeIdentifier;
+
+            // compute hash of project to see if it is different, such as new nuget packages added or removed
+            string projectHash;
+            using (var sha256 = SHA256.Create())
+            {
+                projectHash = Convert.ToBase64String(sha256.ComputeHash(File.ReadAllBytes(pathToProjectFile)));
+            }
+
+            // get old hash code
+            string hashFile = pathToProjectFile + ".hash";
+            if (File.Exists(hashFile) && (File.ReadAllText(hashFile) == projectHash))
+            {
+                // if the same no need to do dotnet restore, package is identical
+                return;
+            }
+
             _logger.Debug($"Restoring {pathToProjectFile} using the dotnet cli. RuntimeIdentifier : {runtimeIdentifier}");
             var exitcode = _commandRunner.Execute("dotnet", $"restore \"{pathToProjectFile}\" -r {runtimeIdentifier} {packageSourcesArgument}");
             if (exitcode != 0)
@@ -30,6 +48,9 @@ namespace Dotnet.Script.DependencyModel.Context
                 // We must throw here, otherwise we may incorrectly run with the old 'project.assets.json'
                 throw new Exception($"Unable to restore packages from '{pathToProjectFile}'. Make sure that all script files contains valid NuGet references");
             }
+
+            // save projectHash for next time
+            File.WriteAllText(hashFile, projectHash);
 
             string CreatePackageSourcesArguments()
             {
@@ -40,6 +61,6 @@ namespace Dotnet.Script.DependencyModel.Context
             }
         }
 
-        public bool CanRestore => _commandRunner.Execute("dotnet", "--version") == 0;
+        public bool CanRestore => true;
     }
 }
