6b3746bc89cdefeb9906c088af782060cee90939
Tom Laird-McConnell <tomlm@microsoft.com>
2018-09-25 08:31:35 -0700

    removed dotnet restore caching, it was breaking unit tests.  I think with the dll caching this is less important, and perhaps explored in different delta

--- src/Dotnet.Script.DependencyModel/Context/DotnetRestorer.cs
+++ src/Dotnet.Script.DependencyModel/Context/DotnetRestorer.cs
@@ -2,9 +2,7 @@
 using Dotnet.Script.DependencyModel.Logging;
 using Dotnet.Script.DependencyModel.Process;
 using System;
-using System.IO;
 using System.Linq;
-using System.Security.Cryptography;
 
 namespace Dotnet.Script.DependencyModel.Context
 {
@@ -26,21 +24,6 @@ namespace Dotnet.Script.DependencyModel.Context
             var packageSourcesArgument = CreatePackageSourcesArguments();
             var runtimeIdentifier = _scriptEnvironment.RuntimeIdentifier;
 
-            // compute hash of project to see if it is different, such as new nuget packages added or removed
-            string projectHash;
-            using (var sha256 = SHA256.Create())
-            {
-                projectHash = Convert.ToBase64String(sha256.ComputeHash(File.ReadAllBytes(pathToProjectFile)));
-            }
-
-            // get old hash code
-            string hashFile = pathToProjectFile + ".hash";
-            if (File.Exists(hashFile) && (File.ReadAllText(hashFile) == projectHash))
-            {
-                // if the same no need to do dotnet restore, package is identical
-                return;
-            }
-
             _logger.Debug($"Restoring {pathToProjectFile} using the dotnet cli. RuntimeIdentifier : {runtimeIdentifier}");
             var exitcode = _commandRunner.Execute("dotnet", $"restore \"{pathToProjectFile}\" -r {runtimeIdentifier} {packageSourcesArgument}");
             if (exitcode != 0)
@@ -48,9 +31,6 @@ namespace Dotnet.Script.DependencyModel.Context
                 // We must throw here, otherwise we may incorrectly run with the old 'project.assets.json'
                 throw new Exception($"Unable to restore packages from '{pathToProjectFile}'. Make sure that all script files contains valid NuGet references");
             }
-
-            // save projectHash for next time
-            File.WriteAllText(hashFile, projectHash);
 
             string CreatePackageSourcesArguments()
             {
