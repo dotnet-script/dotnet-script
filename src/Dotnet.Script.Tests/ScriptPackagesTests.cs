a9cc46afc01e6da733a0c5d5006e14a847b57740
Bernhard Richter <bernhard.richter@gmail.com>
2018-11-02 14:48:27 +0100

    Run script packages tests without the cache

--- src/Dotnet.Script.Tests/ScriptPackagesTests.cs
+++ src/Dotnet.Script.Tests/ScriptPackagesTests.cs
@@ -101,7 +101,7 @@ namespace Dotnet.Script.Tests
                 Console.SetError(stringWriter);
                 var baseDir = AppDomain.CurrentDomain.BaseDirectory;
                 var fullPathToScriptFile = Path.Combine(baseDir, "..", "..", "..", "TestFixtures", "ScriptPackage", scriptFileName);
-                Program.Main(new[] { fullPathToScriptFile });
+                Program.Main(new[] { fullPathToScriptFile , "--nocache"});
                 return output.ToString();
 
             }
