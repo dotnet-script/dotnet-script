6ae3bc925145b7771ace9f3428ff4bcd5d374401
Bernhard Richter <bernhard.richter@gmail.com>
2019-06-20 15:55:23 +0200

    Use unified launch file when installed as a global tool

--- src/Dotnet.Script.Core/Scaffolder.cs
+++ src/Dotnet.Script.Core/Scaffolder.cs
@@ -75,7 +75,7 @@ namespace Dotnet.Script.Core
         {
             if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
             {
-                // register dotnet-script as the tool to process .csx files 
+                // register dotnet-script as the tool to process .csx files
                 _commandRunner.Execute("reg", @"add HKCU\Software\classes\.csx /f /ve /t REG_SZ /d dotnetscript");
                 _commandRunner.Execute("reg", $@"add HKCU\Software\Classes\dotnetscript\Shell\Open\Command /f /ve /t REG_EXPAND_SZ /d ""\""%ProgramFiles%\dotnet\dotnet.exe\"" script \""%1\"" -- %*""");
             }
@@ -136,29 +136,57 @@ namespace Dotnet.Script.Core
             _scriptConsole.WriteNormal("Creating VS Code launch configuration file");
             string pathToLaunchFile = Path.Combine(vsCodeDirectory, "launch.json");
             string installLocation = _scriptEnvironment.InstallLocation;
+            bool isInstalledAsGlobalTool = installLocation.Contains(".dotnet/tools", StringComparison.OrdinalIgnoreCase);
             string dotnetScriptPath = Path.Combine(installLocation, "dotnet-script.dll").Replace(@"\", "/");
+            string launchFileContent = null;
             if (!File.Exists(pathToLaunchFile))
             {
-                string launchFileTemplate = TemplateLoader.ReadTemplate("launch.json.template");
-                string launchFileContent = launchFileTemplate.Replace("PATH_TO_DOTNET-SCRIPT", dotnetScriptPath);
+                if (isInstalledAsGlobalTool)
+                {
+                    launchFileContent = TemplateLoader.ReadTemplate("globaltool.launch.json.template");
+                }
+                else
+                {
+                    string launchFileTemplate = TemplateLoader.ReadTemplate("launch.json.template");
+                    launchFileContent = launchFileTemplate.Replace("PATH_TO_DOTNET-SCRIPT", dotnetScriptPath);
+                }
+
                 File.WriteAllText(pathToLaunchFile, launchFileContent);
                 _scriptConsole.WriteSuccess($"...'{pathToLaunchFile}' [Created]");
             }
             else
             {
                 _scriptConsole.WriteHighlighted($"...'{pathToLaunchFile}' already exists' [Skipping]");
-                var launchFileContent = File.ReadAllText(pathToLaunchFile);
-                string pattern = @"^(\s*"")(.*dotnet-script.dll)("").*$";
-                if (Regex.IsMatch(launchFileContent, pattern, RegexOptions.Multiline))
+                launchFileContent = File.ReadAllText(pathToLaunchFile);
+                if (isInstalledAsGlobalTool)
                 {
-                    var newLaunchFileContent = Regex.Replace(launchFileContent, pattern, $"$1{dotnetScriptPath}$3", RegexOptions.Multiline);
-                    if (launchFileContent != newLaunchFileContent)
+                    var template = TemplateLoader.ReadTemplate("globaltool.launch.json.template");
+                    if (template != launchFileContent)
                     {
-                        _scriptConsole.WriteHighlighted($"...Fixed path to dotnet-script: '{dotnetScriptPath}' [Updated]");
-                        File.WriteAllText(pathToLaunchFile, newLaunchFileContent);
+                        File.WriteAllText(pathToLaunchFile, template);
+                        _scriptConsole.WriteHighlighted("...Use global tool [Updated]");
                     }
                 }
+                else
+                {
+                    string pattern = @"^(\s*"")(.*dotnet-script.dll)("").*$";
+                    if (Regex.IsMatch(launchFileContent, pattern, RegexOptions.Multiline))
+                    {
+                        var newLaunchFileContent = Regex.Replace(launchFileContent, pattern, $"$1{dotnetScriptPath}$3", RegexOptions.Multiline);
+                        if (launchFileContent != newLaunchFileContent)
+                        {
+                            _scriptConsole.WriteHighlighted($"...Fixed path to dotnet-script: '{dotnetScriptPath}' [Updated]");
+                            File.WriteAllText(pathToLaunchFile, newLaunchFileContent);
+                        }
+                    }
+                }
+
             }
+        }
+
+        private bool IsInstalledAsGlobalTool()
+        {
+            return _scriptEnvironment.InstallLocation.Contains(".dotnet/tools", StringComparison.OrdinalIgnoreCase);
         }
     }
 }
