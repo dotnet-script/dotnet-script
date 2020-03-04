ae1577da1832baa78c05b29e4e8b0dd5382f27ab
Bernhard Richter <bernhard.richter@gmail.com>
2020-03-04 12:24:33 +0100

    Use platform spesific temp folders

--- src/Dotnet.Script.DependencyModel/ProjectSystem/FileUtils.cs
+++ src/Dotnet.Script.DependencyModel/ProjectSystem/FileUtils.cs
@@ -1,7 +1,9 @@
 ﻿using Dotnet.Script.DependencyModel.Environment;
+
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Runtime.InteropServices;
 using System.Text;
 
 namespace Dotnet.Script.DependencyModel.ProjectSystem
@@ -27,7 +29,7 @@ namespace Dotnet.Script.DependencyModel.ProjectSystem
                 throw new ArgumentOutOfRangeException(nameof(targetDirectory), "Must be a root path");
             }
 
-            var tempDirectory = Path.GetTempPath();
+            var tempDirectory = GetTempPath();
             var pathRoot = Path.GetPathRoot(targetDirectory);
             var targetDirectoryWithoutRoot = targetDirectory.Substring(pathRoot.Length);
             if (pathRoot.Length > 0 && ScriptEnvironment.Default.IsWindows)
@@ -41,8 +43,25 @@ namespace Dotnet.Script.DependencyModel.ProjectSystem
 
                 targetDirectoryWithoutRoot = Path.Combine(driveLetter, targetDirectoryWithoutRoot);
             }
-            var pathToProjectDirectory = Path.Combine(tempDirectory, "scripts", targetDirectoryWithoutRoot);
+            var pathToProjectDirectory = Path.Combine(tempDirectory, "dotnet-script", targetDirectoryWithoutRoot);
             return pathToProjectDirectory;
+        }
+
+        private static string GetTempPath()
+        {
+            var userFolder = System.Environment.GetFolderPath(System.Environment.SpecialFolder.UserProfile);
+
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+            {
+                return Path.Combine(userFolder, ".cache");
+            }
+            else
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+            {
+                return Path.Combine(userFolder, "Library/Caches/");
+            }
+
+            return Path.GetTempPath();
         }
     }
 }
