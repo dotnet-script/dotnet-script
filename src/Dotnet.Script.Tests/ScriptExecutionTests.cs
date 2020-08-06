3ced1e1ff9cf0773f81e51edf781bc2bc2f43814
Atif Aziz <code@raboof.com>
2020-08-06 16:06:17 +0200

    Test remote execution of URL of a gzipped script

--- src/Dotnet.Script.Tests/ScriptExecutionTests.cs
+++ src/Dotnet.Script.Tests/ScriptExecutionTests.cs
@@ -241,12 +241,15 @@ namespace Dotnet.Script.Tests
             Assert.Contains("AutoMapper.MapperConfiguration", result.output);
         }
 
-        [Fact]
-        public void ShouldExecuteRemoteScript()
+        [Theory]
+        [InlineData("https://gist.githubusercontent.com/seesharper/5d6859509ea8364a1fdf66bbf5b7923d/raw/0a32bac2c3ea807f9379a38e251d93e39c8131cb/HelloWorld.csx",
+                    "Hello World")]
+        [InlineData("https://github.com/filipw/dotnet-script/files/5035247/hello.csx.gz",
+                    "Hello, world!")]
+        public void ShouldExecuteRemoteScript(string url, string output)
         {
-            var url = "https://gist.githubusercontent.com/seesharper/5d6859509ea8364a1fdf66bbf5b7923d/raw/0a32bac2c3ea807f9379a38e251d93e39c8131cb/HelloWorld.csx";
             var result = ScriptTestRunner.Default.Execute(url);
-            Assert.Contains("Hello World", result.output);
+            Assert.Contains(output, result.output);
         }
 
         [Fact]
