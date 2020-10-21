715cc19db5ad6fa007f824f5a65ad150b0c49c5c
Daniel Egbers <daniel@egbers.site>
2020-10-22 01:29:07 +0200

    include command output in restore exception message

--- src/Dotnet.Script.DependencyModel/Context/DotnetRestorer.cs
+++ src/Dotnet.Script.DependencyModel/Context/DotnetRestorer.cs
@@ -31,11 +31,13 @@ namespace Dotnet.Script.DependencyModel.Context
 
             _logger.Debug($"Restoring {projectFileInfo.Path} using the dotnet cli. RuntimeIdentifier : {runtimeIdentifier} NugetConfigFile: {projectFileInfo.NuGetConfigFile}");
 
-            var exitcode = _commandRunner.Execute("dotnet", $"restore \"{projectFileInfo.Path}\" -r {runtimeIdentifier} {packageSourcesArgument} {configFileArgument}", workingDirectory);
-            if (exitcode != 0)
+            var commandPath = "dotnet";
+            var commandArguments = $"restore \"{projectFileInfo.Path}\" -r {runtimeIdentifier} {packageSourcesArgument} {configFileArgument}";
+            var commandResult = _commandRunner.Capture(commandPath, commandArguments, workingDirectory);
+            if (commandResult.ExitCode != 0)
             {
                 // We must throw here, otherwise we may incorrectly run with the old 'project.assets.json'
-                throw new Exception($"Unable to restore packages from '{projectFileInfo.Path}'. Make sure that all script files contains valid NuGet references");
+                throw new Exception($"Unable to restore packages from '{projectFileInfo.Path}'{System.Environment.NewLine}Make sure that all script files contains valid NuGet references{System.Environment.NewLine}{System.Environment.NewLine}Details:{System.Environment.NewLine}{workingDirectory} : {commandPath} {commandArguments}{System.Environment.NewLine}{commandResult.StandardOut}");
             }
 
             string CreatePackageSourcesArguments()
