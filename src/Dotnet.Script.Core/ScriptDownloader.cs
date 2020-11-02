0899829e0d433cc71dc4c34e2c977ca361c27b02
Andrew Jordan <andrew.m.jordan@hotmail.com>
2020-11-01 16:57:21 -0800

    Support remote scripts with empty/null Content Type

--- src/Dotnet.Script.Core/ScriptDownloader.cs
+++ src/Dotnet.Script.Core/ScriptDownloader.cs
@@ -18,7 +18,7 @@ namespace Dotnet.Script.Core
 
                     using (HttpContent content = response.Content)
                     {
-                        var mediaType = content.Headers.ContentType.MediaType?.ToLowerInvariant().Trim();
+                        var mediaType = content.Headers.ContentType?.MediaType?.ToLowerInvariant().Trim();
                         switch (mediaType)
                         {
                             case null:
